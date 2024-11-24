188e863c808d75ab56de43d84ac4f7683686fb7c
Arun silawat <arunsilawat722362@gmail.com>
2024-11-24 18:04:36 +0530

    add to cart

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,9 @@ from app import views
 urlpatterns = [
     path('base/', views.base, name='base'),
     path('', views.home, name='home'),
+    path('datapost/', views.datapost, name='datapost'),
+    path("addtocard/<int:pk>",views.addtocard,name='addtocard'),
+    path("cart/",views.cart,name="cart"),
+    path("deletecart/<int:pk>",views.deletecart,name='deletecart'),
+    path("details/<int:pk>",views.details,name="details"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,130 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import ItemInfo
+from .forms import ItemInfoForm
+# Create your views here.
 
+# import razorpay
+# from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 def base(request):
     return render(request,'base.html')
 def home(request):
-    return render(request,'home.html')
+    data = ItemInfo.objects.all()
+    return render(request,'home.html',{'data':data})
+
+def datapost(request):
+    if request.method=="POST":
+        form = ItemInfoForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+        data = ItemInfo.objects.all()
+        return render(request,'datapost.html',{'form':form,'data':data})
+    form = ItemInfoForm()
+    data = ItemInfo.objects.all()
+    if data:
+        return render(request,'datapost.html',{'form':form,'data':data})
+    else:
+        return render(request,'datapost.html',{'form':form})
+def addtocard(request,pk):
+    if request.method == 'POST':
+        quantity = request.session.get('quantity', [])
+        quantity1 =int(request.POST.get('quantity'))
+        quantity.append(quantity1)
+        # print("quantity :",quantity)
+        request.session['quantity'] = quantity
+        cart = request.session.get('cart', [])
+        cart.append(pk)
+        request.session['cart'] = cart
+        # form = ItemInfoForm()
+        data = ItemInfo.objects.all()
+        return render(request,'home.html',{ 'data':data})
+def cart(request):
+    cart = request.session.get('cart',[])
+    quantity = request.session.get('quantity',[])
+    # print("Cart :",cart)
+    # print("Quantity :",quantity)
+    # print(len(cart))
+    alldata = []
+    i=0
+    j=0
+    total=0
+    while i < len(cart):
+        data = ItemInfo.objects.get(id=cart[i])
+        total = total + (data.item_price)*quantity[j]
+        # print(data.id)
+        # print(data.iten_name)
+        # print(data.item_desc)
+        # print(data.item_price)
+        # print(data.item_image)
+        alldata.append({
+            'id':data.id,
+            'iten_name':data.iten_name,
+            'item_details':data.item_details,
+            'item_price':data.item_price,
+            'item_image':data.item_image,
+            'item_quantity':quantity[j]
+        })
+        i+=1
+        j+=1
+    # print("Total Amount = ",total)
+    # print(alldata)
+    return render(request,'cart.html',{'key':alldata,'amount':total})
+def deletecart(request,pk):
+    cart = request.session.get('cart',[])
+    quantity = request.session.get('quantity',[])
+ 
+    x = cart.index(pk)
+    # print("Cart index no:",x)
+    # y = quantity[x]
+    # print("Quantity of that card index:",y)
+    cart1=[]
+    y = len(cart)   
+    i=0
+    while i<y:
+        if i==x:
+            pass
+        else:
+            cart1.append(cart[i])
+        i+=1
+    request.session['cart']=cart1
+    quantity1=[]
+    z = len(quantity)
+    j=0
+    while j<z:
+        if j==x:
+            pass
+        else:
+            quantity1.append(quantity[j])
+        j+=1
+    request.session['quantity']=quantity1
+    # ----------------------------------------------------
+    cart = request.session.get('cart',[])
+    quantity = request.session.get('quantity',[])
+    # print(len(cart))
+    alldata = []
+    i=0
+    j=0
+    total=0
+    while i < len(cart):
+        data = ItemInfo.objects.get(id=cart[i])
+        total = total + (data.item_price)*quantity[j]
+        # print(data.id)
+        # print(data.item_name)
+        # print(data.item_desc)
+        # print(data.item_price)
+        # print(data.item_image)
+        alldata.append({
+            'id':data.id,
+            'iten_name':data.iten_name,
+            'item_details':data.item_details,
+            'item_price':data.item_price,
+            'item_image':data.item_image,
+            'item_quantity':quantity[j]
+        })
+        i+=1
+        j+=1
+    # print("Total Amount = ",total)
+    return render(request,'cart.html',{'key':alldata,'amount':total})
+def details(request,pk):
+    data=ItemInfo.objects.get(id=pk)
+    return render(request,'details.html',{'data':data})
